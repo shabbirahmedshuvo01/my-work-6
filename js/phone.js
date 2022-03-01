e22d398a1de0a18b79b189e7682afa64d13eefb9
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-03-01 14:53:33 +0600

    error are done

--- js/phone.js
+++ js/phone.js
@@ -1,16 +1,29 @@
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    // console.log(searchText);
+    // clear
     searchField.value = '';
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchPhone(data.data))
+    if (searchText == '') {
+        alert('please write somthing')
+    }
+
+    else {
+        // loadsedding
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchPhone(data.data))
+    }
+
+
 }
 
 const displaySearchPhone = phones => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    if (phones.length == 0) {
+        alert('No phone found')
+    }
     phones.forEach(phone => {
         // console.log(phone);
         const div = document.createElement('div');
@@ -41,6 +54,7 @@ const loadPhoneDetail = phoneId => {
 const displayPhoneDetails = phone => {
     console.log(phone);
     const phoneSpecification = document.getElementById('specification');
+    phoneSpecification.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
